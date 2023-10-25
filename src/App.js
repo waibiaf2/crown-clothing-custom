921c25873587a28ca6205f67e4ce67d838bac06b
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-10-25 18:05:23 +0300

    add authentication, sign in and sign out related components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import HomeComponent from "./routes/home/home.component";
 import NavigationComponent from "./routes/navigation/navigation.component";
 import ShopComponent from "./routes/shop/shop.component";
+import SingInComponent from "./routes/sing-in/sing-in.component";
+import AuthenticationComponent from "./routes/authentication/authentication.component";
 
 function App() {
    
@@ -13,6 +15,7 @@ function App() {
                 <Route path='/' element={<NavigationComponent/>}>
                     <Route index element={<HomeComponent/>}/>
                     <Route path='/shop' element={<ShopComponent/>}/>
+                    <Route path='/auth' element={<AuthenticationComponent/>}/>
                 </Route>
             </Routes>
         </div>
---
+++ src/routes/sing-in/sing-in.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {
+	createUserDocumentFromAuth,
+	signInWithGooglePopup
+} from "../../utils/firebase.utils";
+import SignUpFormComponent from "../../components/sign-up-form/sign-up-form.component";
+
+
+
+const SingInComponent = () => {
+	const logGoogleUer = async () => {
+		const {user} = await signInWithGooglePopup();
+		console.log(user)
+		const userDocRef = await createUserDocumentFromAuth(user);
+		console.log(userDocRef);
+	}
+	return (
+		<div>
+			<h1>Sing In Page</h1>
+			<button onClick={logGoogleUer}>Sing in With Google Popup</button>
+			
+			<SignUpFormComponent/>
+		</div>
+	);
+};
+
+export default SingInComponent;
