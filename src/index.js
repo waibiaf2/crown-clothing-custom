0107e834ae556ee5390424afbcbfeeabc6f6db81
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-10-30 10:01:12 +0300

    mount categories provider to index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ import {BrowserRouter} from "react-router-dom";
 import {UserProvider} from "./contexts/user.context";
 import {ProductProvider} from "./contexts/products.context";
 import {CartProvider} from "./contexts/cart.context";
+import {CategoriesProvider} from "./contexts/catagories.context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
           <UserProvider>
-              <ProductProvider>
+              <CategoriesProvider>
                   <CartProvider>
                       <App />
                   </CartProvider>
-              </ProductProvider>
+              </CategoriesProvider>
           </UserProvider>
       </BrowserRouter>
   </React.StrictMode>
