8f43981b16abc0fe3cd095f1232ef59bf088a94f
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-11-10 18:10:33 +0300

    remove categories context provider in the index.js

--- src/index.js
+++ src/index.js
@@ -7,23 +7,20 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 import {CartProvider} from "./contexts/cart.context";
-import {CategoriesProvider} from "./contexts/catagories.context";
 import {store} from "./store/store";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-	 <Provider store={store}>
-		 <BrowserRouter>
-				 <CategoriesProvider>
-					 <CartProvider>
-						 <App />
-					 </CartProvider>
-				 </CategoriesProvider>
-		 </BrowserRouter>
-	 </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<CartProvider>
+					<App/>
+				</CartProvider>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
